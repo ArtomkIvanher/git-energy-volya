b19e795e9c32940081814b4a42c985f023fdd1f4
ArtomkIvanher <artomk909@gmail.com>
2024-04-29 19:13:40 +0300

    add volya

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { Layout } from "antd";
+import CompSider from "./components/CompSider/CompSider";
+import CompContent from "./components/CompContent/CompContent";
+import But from "./components/Button/Button";
+import { useState } from "react";
+import { dataE } from "./data";
+import "./components/Button/Button.css"
+import CompFooter from "./components/CompFooter/CompFooter";
+import "./components/CompColor.css"
 
-function App() {
+export default function App() {
+  const [stateBtn, setStateBtn] = useState('red')
+
+  function handleClick(type) {
+    setStateBtn(type)
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edigg <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout style={{ display: "flex", flexDirection: "row", maxHeight: "100vh" }}>
+      <CompSider>{stateBtn}</CompSider>
+      <Layout className={stateBtn}>
+        <Layout.Header style={{height: "60px", zIndex: 100}} className="header">
+          <ul className="btn-linia">
+            <But onClick={() => handleClick("red")}>{dataE.redName}</But>
+            <But onClick={() => handleClick("green")}>{dataE.greenName}</But>
+            <But onClick={() => handleClick("yellow")}>{dataE.yellowName}</But>
+            <But onClick={() => handleClick("pink")}>{dataE.pinkName}</But>
+          </ul>
+        </Layout.Header>
+        <CompContent>{stateBtn}</CompContent>
+        <CompFooter>{stateBtn}</CompFooter>
+      </Layout>
+    </Layout>
   );
 }
-
-export default App;
