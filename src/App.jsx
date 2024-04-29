d61d8b61d4d2aa76fd74c7b8b1b6574e7f0fab3e
ArtomkIvanher <artomk909@gmail.com>
2024-04-29 18:43:16 +0300

    changed file index

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Editvvvv <code>src/App.js</code> and save to reload.
+          Edigg <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
